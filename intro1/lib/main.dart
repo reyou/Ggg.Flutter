494ad096ba946897465bf8d151e1f7b745a9f716
teoman shipahi <aytekoz@yahoo.com>
2022-01-22 00:19:16 -0600

    action button

--- intro1/lib/main.dart
+++ intro1/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('hey ninjas!'),
-  ));
+  runApp(MaterialApp(home: Scaffold(
+    appBar: AppBar(
+      title: Text("ReadEndless"),
+      centerTitle: true,
+    ),
+    body: Center(child: Text("Read books!"),),
+    floatingActionButton: FloatingActionButton(
+      child: Text("Crypto"),
+      onPressed: () {
+
+      },
+    ),
+  )));
 }
- 
