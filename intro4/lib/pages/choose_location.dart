605411540c2d5457b291af40121c1ad2b094c4a7
teoman shipahi <aytekoz@yahoo.com>
2022-01-27 00:05:38 -0600

    lifecycle

--- intro4/lib/pages/choose_location.dart
+++ intro4/lib/pages/choose_location.dart
@@ -8,8 +8,17 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("initState function run");
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("build function run");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
